957e071547f9b0dc0336b73813fee2d05182c091
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-11-25 12:49:47 +0700

    add play and stop button and config

--- app/src/main/java/com/example/mymediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mymediaplayer/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.mymediaplayer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = MainActivity::class.java.simpleName
+    private var mService: Messenger? = null
+    private lateinit var mBoundServiceIntent: Intent
+    private var mServiceBound = false
     private var mMediaPlayer: MediaPlayer? = null
     private var isReady: Boolean = false
+
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            mService = null
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            mService = Messenger(p1)
+            mServiceBound = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,29 +42,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btn_play.setOnClickListener(this)
         btn_stop.setOnClickListener(this)
 
-        init()
+        mBoundServiceIntent = Intent(this, MediaService::class.java)
+        mBoundServiceIntent.action = MediaService.ACTION_CREATE
+
+        startService(mBoundServiceIntent)
+        bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
     }
 
     override fun onClick(p0: View) {
         val id = p0.id
         when (id) {
-            R.id.btn_play -> if (!isReady) {
-                mMediaPlayer?.prepareAsync()
-            } else {
-                if (mMediaPlayer?.isPlaying() as Boolean) {
-                    mMediaPlayer?.pause()
-                } else {
-                    mMediaPlayer?.start()
+            R.id.btn_play -> {
+                if (!mServiceBound) return
+                try {
+                    mService?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
-            R.id.btn_stop -> if (mMediaPlayer?.isPlaying() as Boolean || isReady) {
-                mMediaPlayer?.stop()
-                isReady = false
+            R.id.btn_stop -> {
+                if (!mServiceBound) return
+                try {
+                    mService?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
             }
             else -> {
-
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(mServiceConnection)
+        mBoundServiceIntent.action = MediaService.ACTION_DESTROY
+
+        startService(mBoundServiceIntent)
     }
 
     private fun init() {
